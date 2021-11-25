6c7550d552c82de8f4c8c5e7935baf4fef6331aa
Roman Rakhlin <rrakhlin@gmail.com>
2021-11-25 10:09:43 +0300

    replace project UIKit to new with SwiftUI

---
+++ It'sMoreThanAScheduller/It_sMoreThanASchedullerApp.swift
@@ -0,0 +1,17 @@
+//
+//  It_sMoreThanASchedullerApp.swift
+//  It'sMoreThanAScheduller
+//
+//  Created by Roman Rakhlin on 25.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct It_sMoreThanASchedullerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
