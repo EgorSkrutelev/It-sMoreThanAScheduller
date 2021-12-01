2dea03056e159d1a8de16947f6f431bf0c05a619
Николай Циминтия <nikolajcimintia@MacBook-Pro-Nikolaj-2.local>
2021-12-01 15:53:29 +0300

    I did the login page

--- It'sMoreThanAScheduller/ContentView.swift
+++ It'sMoreThanAScheduller/ContentView.swift
@@ -1,21 +1,75 @@
 //
 //  ContentView.swift
-//  It'sMoreThanAScheduller
+//  LogInPageOfSchedule
 //
-//  Created by Roman Rakhlin on 25.11.2021.
+//  Created by Николай Циминтия on 25.11.2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(CGColor(red: 0.79, green: 0.84, blue: 0.93, alpha: 1.00))
+                .ignoresSafeArea()
+            VStack {
+                Text("")
+                    .padding()
+                Image("itmo_horiz_white_eng")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                IdentifierTextField()
+                PasswordTextField()
+                LoginButton()
+
+                
+            }.padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 13"))
     }
+}
+
+struct IdentifierTextField: View {
+    @State var text = ""
+    
+    var body: some View {
+        TextField(" ISU ID", text: $text)
+            .padding()
+            .background()
+            .frame(width: 300, height: nil, alignment: .center)
+            .cornerRadius(25)
+    }
+    
+}
+
+struct PasswordTextField: View {
+    @State var text = ""
+    
+    var body: some View {
+        SecureField("Password", text: $text, prompt: nil)
+            .padding()
+            .background()
+            .frame(width: 300, height: nil, alignment: .center)
+            .cornerRadius(25)
+    }
+    
+}
+
+struct LoginButton: View {
+    
+    var body: some View {
+        Button("Log in") {
+            print("lol")
+        }
+        .frame(width: 100, height: 40, alignment: .center)
+        .background(Color.red)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+    
 }
